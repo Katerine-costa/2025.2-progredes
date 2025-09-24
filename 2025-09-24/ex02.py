c0af36cf21df8d9363f9b9f1df63e9686c6e4303
katerine-costa <arielekaterine206@gmail.com>
2025-09-24 17:46:33 -0300

    binarios

---
+++ 2025-09-24/ex02.py
@@ -0,0 +1,19 @@
+# gerar a mascara  -------  # deslocamento binario
+
+intCIDR = 24
+
+intMask = 0xffffffff
+
+intMask = intMask >> (32 - intCIDR)   # deslocamento binario
+
+intMask = intMask << (32 - intCIDR)   # deslocamento binario
+
+print(intMask)
+
+# Será impresso -> 4294967040 (4.294.967.040)
+
+# ----------------------------------------------------------- #
+
+# Forma simplificada #
+
+# intMask = 0xffffffff >> (32 - intCIDR) << (32 - intCIDR)
