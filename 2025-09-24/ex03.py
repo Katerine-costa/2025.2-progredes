b0b6f5c5635f8f1015fc43cd5b62db46fcaf114e
katerine-costa <arielekaterine206@gmail.com>
2025-09-24 17:59:00 -0300

    ip e mascara

---
+++ 2025-09-24/ex03.py
@@ -0,0 +1,9 @@
+# ip e mascara 
+
+strIP   = '192.168.1.10'
+intCIDR = 24
+
+
+intIP = int.from_bytes(bytes([int(x) for x in strIP.split('.')]) ,'big')
+
+intMascara = 0xFFFFFFFF >> (32 - intCIDR) << (32 - intCIDR) # forma simplificada
