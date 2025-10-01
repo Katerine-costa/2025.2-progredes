bbe311c2de1ff3e0de720bf75084c77fe02cca4e
katerine-costa <arielekaterine206@gmail.com>
2025-10-01 18:03:39 -0300

    a

---
+++ 2025-10-01/ex01.py
@@ -0,0 +1,10 @@
+# Recapitulando
+intValor_1 = 195
+intValor_2 = 89
+
+print(f'Valor 1................: {intValor_1:>3} -> {intValor_1:08b}')
+print(f'Valor 2................: {intValor_2:>3} -> {intValor_2:08b}')
+
+print(f'intValor_1 & intValor_2: {intValor_1 & intValor_2:>3} -> {(intValor_1 & intValor_2):08b}')
+print(f'intValor_1 | intValor_2: {intValor_1 | intValor_2:>3} -> {(intValor_1 | intValor_2):08b}')
+print(f'intValor_1 ^ intValor_2: {intValor_1 ^ intValor_2:>3} -> {(intValor_1 ^ intValor_2):08b}')
